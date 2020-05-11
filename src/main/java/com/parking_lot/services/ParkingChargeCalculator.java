6f0c8790d818bff43cf400fc29c81a1299a6e167
poojbhat <pooja.bhatia@amdocs.com>
2020-05-11 14:19:23 +0530

    minor changes

--- src/main/java/com/parking_lot/services/ParkingChargeCalculator.java
+++ src/main/java/com/parking_lot/services/ParkingChargeCalculator.java
@@ -1,11 +1,11 @@
 package com.parking_lot.services;
 
-public class ParkingChargeCalculator {
+class ParkingChargeCalculator {
 
     private static final int CHARGE = 10;
     private static final int MINIMUM_HOURS = 2;
 
-    public static int calculateCharge(int hours) {
+    static int calculateCharge(int hours) {
         if (hours <= MINIMUM_HOURS) {
             return CHARGE;
         }
